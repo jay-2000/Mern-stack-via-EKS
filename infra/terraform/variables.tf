4d1f2ff8ae41abcaaa4ff5db90a2d0f9385b705d
jay-2000 <jayparmar7654321@gmail.com>
2025-12-20 10:30:46 +0530

    cleaing unwanted files

--- infra/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region"
-  default     = "ap-south-1"
-}
-
-variable "cluster_name" {
-  description = "EKS Cluster Name"
-  default     = "cheap-eks-demo"
-}
